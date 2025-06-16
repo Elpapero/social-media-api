b16d1989c6bca6cb26bd1e963bcd4282ddb17b19
Gustavo-Zamai <gustavosimaozamai@gmail.com>
2025-06-16 12:44:51 -0300

    Add the register and list functionalities

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,31 @@
-import express from 'express';
-// import routes from './routes/routes';
-import cors from 'cors';
+import express from "express";
+import routes from './routes/routes';
+import cors from "cors";
+import { prisma } from './utils/prisma';
 
 function createApp() {
   const app = express();
   app.use(express.json());
 
-  // app.use('/api', routes);
+  app.use(express.json()); 
+  app.get("/users", async (req, res) => {
+    const users = await prisma.user.findMany({ include: { posts: true } });
+    res.json(users);
+  });
+
+  app.post("/users", async (req, res) => {
+    const { name, email, password } = req.body;
+    const user = await prisma.user.create({
+      data: { name, email, password },
+    });
+    res.status(201).json(user);
+  });
+
+  app.use('/api', routes);
 
   const corsOptions = {
     origin: "*",
-    methods: ["GET", "POST", "PATCH", "DELETE"]
+    methods: ["GET", "POST", "PATCH", "DELETE"],
   };
 
   app.use(cors(corsOptions));
